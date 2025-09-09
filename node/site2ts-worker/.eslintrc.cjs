7d56b8fd94ebf54e0d64710feb874c0b8f28c8dd
glassfox <glassfox@gmail.com>
2025-09-09 15:27:15 +0300

    ci(node): fix ESLint for TypeScript and make install resilient; set author to glassfox

--- node/site2ts-worker/.eslintrc.cjs
+++ node/site2ts-worker/.eslintrc.cjs
@@ -1,9 +1,15 @@
 module.exports = {
   root: true,
   env: { es2022: true, node: true },
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['import'],
-  extends: ['eslint:recommended', 'plugin:import/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json', sourceType: 'module' },
+  plugins: ['import', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   rules: {
     'no-console': 'off'
   },
@@ -13,4 +19,3 @@ module.exports = {
     }
   }
 };
-
